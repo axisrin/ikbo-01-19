1d49cb1aff37d30f77692ba9d19fecf6c1cd860f
axisrin <70569999+axisrin@users.noreply.github.com>
2020-09-08 09:59:52 +0300

    Practice_2 -Proshin

---
+++ practice_2 - Proshin/src/practice_2/proshin/Practice_2Proshin.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice_2.proshin;
+
+/**
+ *
+ * @author student
+ */
+public class Practice_2Proshin {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
