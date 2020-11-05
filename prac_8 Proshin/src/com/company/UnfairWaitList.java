8e9e4ce15d5d6a591f5a57883c3fd9416e21b10f
axisrin <70569999+axisrin@users.noreply.github.com>
2020-11-05 18:31:18 +0300

    up to 8 prac

---
+++ prac_8 Proshin/src/com/company/UnfairWaitList.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class UnfairWaitList <E> extends WaitList<E> {
+
+
+
+    public UnfairWaitList(){
+
+        list = new ArrayList<>();
+
+    }
+
+    public void remove(E element) {
+        list.remove(element);
+    }
+
+    public void moveToBack(E element) {
+        list.add(list.get(list.indexOf(element)));
+        list.remove(element);
+    }
+}
